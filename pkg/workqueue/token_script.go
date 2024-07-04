5527f9ecfd91a7e22750cbfe77e791a4511bab98
beihai <beihai0xff@gmail.com>
2024-07-05 01:24:42 +0800

    improve: calculate redis rate-limit token in milliseconds (#22)

--- pkg/workqueue/token_script.go
+++ pkg/workqueue/token_script.go
@@ -16,8 +16,8 @@ local now = tonumber(ARGV[3])
 -- consumed token number
 local consumed = tonumber(ARGV[4])
 
--- how long the bucket will be full
-local fill_time = capacity/rate
+-- how long the bucket will be full, in milliseconds
+local fill_time = 1000*capacity/rate
 -- how long the token will be expired, 2 times of fill_time
 local ttl = math.floor(fill_time*2)
 -- current token number
@@ -36,7 +36,7 @@ end
 
 -- calculate the new token number
 local delta = math.max(0, now-last_refreshed)
-local filled_tokens = math.min(capacity, current_tokens+(delta*rate))
+local filled_tokens = math.min(capacity, current_tokens+(delta*rate/1000))
 -- check if the token is enough
 local allowed = filled_tokens >= consumed
 local new_tokens = filled_tokens
@@ -44,8 +44,8 @@ if allowed then
     new_tokens = filled_tokens - consumed
 end
 
-redis.call("setex", KEYS[1], ttl, new_tokens)
-redis.call("setex", KEYS[2], ttl, now)
+redis.call("PSETEX", KEYS[1], ttl, new_tokens)
+redis.call("PSETEX", KEYS[2], ttl, now)
 
 return allowed
 `)
